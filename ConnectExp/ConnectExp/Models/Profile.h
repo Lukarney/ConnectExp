83dd298c1920ee01a1fdde7cfa2279e194eef4ee
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-07-13 14:09:45 -0500

    add profile model

---
+++ ConnectExp/ConnectExp/Models/Profile.h
@@ -0,0 +1,26 @@
+//
+//  Profile.h
+//  ConnectExp
+//
+//  Created by Luke Arney on 7/13/21.
+//
+
+#import <Parse/Parse.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Profile : PFObject
+
+@property (nonatomic, strong) NSString *profileID;
+@property (nonatomic, strong) NSString *userID;
+@property (nonatomic, strong) PFUser *author;
+
+@property (nonatomic, strong) NSString *biography;
+@property (nonatomic, strong) PFFileObject *image;
+
++ (void) postProfileImage: ( UIImage * _Nullable )image withCaption: ( NSString * _Nullable )caption withCompletion: (PFBooleanResultBlock  _Nullable)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ConnectExp/ConnectExp/Models/Profile.m
@@ -0,0 +1,48 @@
+//
+//  Profile.m
+//  ConnectExp
+//
+//  Created by Luke Arney on 7/13/21.
+//
+
+#import "Profile.h"
+
+@implementation Profile
+
+@dynamic profileID;
+@dynamic userID;
+@dynamic author;
+@dynamic biography;
+@dynamic image;
+
++ (nonnull NSString *)parseClassName {
+    return @"Post";
+}
+
++ (void) postProfileImage: ( UIImage * _Nullable )image withCaption: ( NSString * _Nullable )caption withCompletion: (PFBooleanResultBlock  _Nullable)completion {
+    
+    Profile *newProfile = [Profile new];
+    newProfile.image = [self getPFFileFromImage:image];
+    newProfile.author = [PFUser currentUser];
+    newProfile.biography = caption;
+    
+    [newProfile saveInBackgroundWithBlock: completion];
+}
+
++ (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+ 
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    
+    return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+}
+
+@end
