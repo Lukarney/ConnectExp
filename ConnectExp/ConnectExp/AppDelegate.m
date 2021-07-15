817b62e45c8d4df3c95a1464e3c3dfe101b05d2d
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-07-15 15:29:29 -0500

    adding profiles and messaging layout

--- ConnectExp/ConnectExp/AppDelegate.m
+++ ConnectExp/ConnectExp/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -34,6 +35,20 @@
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(  NSDictionary *)launchOptions {
+
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+        configuration.applicationId = @"YOUR_APP_ID"; // <- UPDATE
+        configuration.clientKey = @"YOUR_CLIENT_KEY"; // <- UPDATE
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+
+    return YES;
 }
 
 
